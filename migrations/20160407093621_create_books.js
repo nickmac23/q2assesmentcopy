ff9dce4100baf697cc7a5b119f2cd0d3d2f25522
Nick MacIntyre <nickmac@Nick-MacBook-Air.local>
2016-04-07 12:16:15 -0600

    seed database

--- migrations/20160407093621_create_books.js
+++ migrations/20160407093621_create_books.js
@@ -1,20 +1,23 @@
 
 exports.up = function(knex, Promise) {
-  return knex.schema
-  // .createTable('genres', function(table){
-  //     table.increments('genre_id');
-  //     table.string('genre');
-  //   })
+  return knex.schema.createTable('authors', function(table){
+    table.increments('author_id');
+    table.string('firstName');
+    table.string('lastName');
+    table.text('biography');
+    table.string('portrait_url');
+  })
     .createTable('books', function(table){
     table.increments('book_id');
     table.string('title');
     table.integer('genre_fk').unsigned().references('genre_id').inTable('genres');
+    table.integer('author_fk').unsigned().references('author_id').inTable('authors');
     table.text('description');
     table.string('cover_url');
   })
 };
 
 exports.down = function(knex, Promise) {
-  // return knex.schema.dropTable('genres');
+  return knex.schema.dropTable('authors');
   return knex.schema.dropTable('books');
 };
--- routes/books.js
+++ routes/books.js
@@ -3,8 +3,7 @@ var router = express.Router();
 var knex = require('knex')(require('../knexfile')['development']);
 
 router.get('/', function(req, res, next) {
-  knex('books').first().then( function(books){
-    var book = books.map
+  knex('books').then( function(books){
     res.render('books', {books: books});
   })
 });
